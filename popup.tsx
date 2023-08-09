11bc1523c53ac53cf6588304409fee6a35212c53
Oscar Arreola <77394565+Oscar-gg@users.noreply.github.com>
2023-08-09 12:23:05 -0600

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -13,8 +13,11 @@ function IndexPopup() {
         <h2 className="text-lg m-2 text-center text-white font-semibold">Convertidor Horario</h2>
       </div>
 
-      <a className="text-blue-600 underline decoration-blue-700 text-center" href="https://docs.plasmo.com" target="_blank">
+      <a className="text-blue-600 underline decoration-blue-700 text-center" href="https://drive.google.com/drive/folders/1vm55XHKsZ1FmipSqHp3riQBzsJ-gsLMn?usp=sharing" target="_blank">
         Ver tutorial
+      </a>
+      <a className="text-blue-600 underline decoration-blue-700 text-center" href="https://github.com/IvanRomero03/rbrgs-convertidor-horario/tree/main" target="_blank">
+        Ver README
       </a>
       <button
         className="rounded-md bg-blue-300 m-5 p-3 text-white text-base"
